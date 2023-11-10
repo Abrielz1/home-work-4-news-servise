7001906a6390caa794a9ecc8e863ab0c34c230b9
Abriel <abrielz11@gmail.com>
2023-11-10 22:24:48 +0300

    created user package and news and commentary

--- src/main/java/ru/skillbox/homework4/news/controller/NewsController.java
+++ src/main/java/ru/skillbox/homework4/news/controller/NewsController.java
@@ -3,6 +3,7 @@ package ru.skillbox.homework4.news.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ru.skillbox.homework4.commentary.service.CommentaryService;
 import ru.skillbox.homework4.news.service.NewsService;
 import ru.skillbox.homework4.user.service.UserService;
 
